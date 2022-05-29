3272ac9c7a484777991bf8ecf6814c1750502855
faintsnov <103983473+faintsnov@users.noreply.github.com>
2022-05-29 21:25:59 +0900

    update features

--- files/whoAmI.lua
+++ files/whoAmI.lua
@@ -11,16 +11,18 @@ local function whoAmI()
         return
     end
 
-    if (NEMESIS.whoamiToken == nil or GameGetFrameNum() % 600 == 0) then
-        if (NEMESIS.whoamiToken == nil) then
-            SetRandomSeed( GameGetFrameNum(), GameGetFrameNum() )
-            local token = ""
-            for i = 1, 32 do
-                token = token .. string.char(Random(97, 122))
-            end
-            NEMESIS.whoamiToken = token
+    if (NEMESIS.whoamiToken == nil) then
+        local year,month,day,hour,minute,second = GameGetDateAndTimeUTC()
+
+        SetRandomSeed( minute, second )
+        local token = ""
+        for i = 1, 32 do
+            token = token .. string.char(Random(97, 122))
         end
-    
+        NEMESIS.whoamiToken = token
+    end
+
+    if (NEMESIS.whoamiToken == nil or GameGetFrameNum() % 600 == 0) then
         local queue = json.decode(NT.wsQueue)
         table.insert(queue, {event="CustomModEvent", payload={name="WhoAmI", token=NEMESIS.whoamiToken}})
         print(" -------------- debug whoAmI called. token:"..NEMESIS.whoamiToken)
