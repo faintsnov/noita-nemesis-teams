8cedd6940a71a5c3045881eb27f1ebecb0d68611
faintsnov <103983473+faintsnov@users.noreply.github.com>
2022-05-30 12:13:00 +0900

    remove unused files

--- files/remove_team_flags.lua
+++
@@ -1,5 +0,0 @@
-
---GameRemoveFlagRun("nt_nemesis_team_deer") 
---GameRemoveFlagRun("nt_nemesis_team_duck")
---GameRemoveFlagRun("nt_nemesis_team_sheep")
---GameRemoveFlagRun("nt_nemesis_team_fungus")
